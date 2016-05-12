feba55bd25ef01d395588ff271f46ee5d5195d9c
Alexey Raga <alexey.raga@gmail.com>
2016-05-12 21:24:53 +1000

    basics of xmk

--- src/HaskellWorks/Data/Xml/Conduit/Blank.hs
+++ src/HaskellWorks/Data/Xml/Conduit/Blank.hs
@@ -6,20 +6,22 @@ module HaskellWorks.Data.Xml.Conduit.Blank
   ) where
 
 import           Control.Monad
-import           Control.Monad.Trans.Resource         (MonadThrow)
-import           Data.ByteString                      as BS
+import           Control.Monad.Trans.Resource        (MonadThrow)
+import           Data.ByteString                     as BS
 import           Data.Conduit
 import           Data.Word
 import           Data.Word8
 import           HaskellWorks.Data.Xml.Conduit.Words
-import           Prelude                              as P
+import           Prelude                             as P
 
 data BlankState
-  = Escaped
-  | InXml
-  | InString
+  = InXml
+  | InElement
+  | InAttrList
+  | InString Word8
   | InNumber
   | InIdent
+  | Test
 
 blankXml :: MonadThrow m => Conduit BS.ByteString m BS.ByteString
 blankXml = blankXml' InXml
@@ -36,28 +38,49 @@ blankXml' lastState = do
   where
     blankByteString :: (BlankState, ByteString) -> Maybe (Word8, (BlankState, ByteString))
     blankByteString (InXml, bs) = case BS.uncons bs of
-      Just (!c, !cs) | isLeadingDigit c   -> Just (_1          , (InNumber , cs))
-      Just (!c, !cs) | c == _quotedbl     -> Just (_parenleft  , (InString , cs))
-      Just (!c, !cs) | isAlphabetic c     -> Just (c           , (InIdent  , cs))
-      Just (!c, !cs)                      -> Just (c           , (InXml   , cs))
-      Nothing -> Nothing
-    blankByteString (InString, bs) = case BS.uncons bs of
-      Just (!c, !cs) | c == _backslash    -> Just (_space      , (Escaped  , cs))
-      Just (!c, !cs) | c == _quotedbl     -> Just (_parenright , (InXml   , cs))
-      Just (_ , !cs)                      -> Just (_space      , (InString , cs))
+      Just (!c, !cs) | c == _less         -> Just (c           , (InElement , cs))
+      Just (!c, !cs)                      -> Just (c           , (InXml     , cs))
       Nothing                             -> Nothing
-    blankByteString (Escaped, bs) = case BS.uncons bs of
-      Just (_, !cs)                       -> Just (_space, (InString, cs))
+    blankByteString (InElement, bs) = case BS.uncons bs of
+      Just (!c, !cs) | c == _greater      -> Just (c           , (InXml     , cs))
+      Just (!c, !cs) | isSpace c          -> Just (_parenleft  , (InAttrList, cs))
+      Just (!c,  !cs)                      -> Just (c      , (InElement , cs))
       Nothing                             -> Nothing
-    blankByteString (InNumber, bs) = case BS.uncons bs of
-      Just (!c, !cs) | isTrailingDigit c  -> Just (_0          , (InNumber , cs))
-      Just (!c, !cs) | c == _quotedbl     -> Just (_parenleft  , (InString , cs))
-      Just (!c, !cs) | isAlphabetic c     -> Just (c           , (InIdent  , cs))
-      Just (!c, !cs)                      -> Just (c           , (InXml   , cs))
+    blankByteString (InAttrList, bs) = case BS.uncons bs of
+      Just (!c, !cs) | c == _greater      -> Just (_parenright , (InXml     , cs))
+      Just (!c, !cs) | c == _slash        -> Just (_parenright , (InElement , cs))
+      Just (!c, !cs) | isNameChar c       -> Just (_parenleft  , (InIdent   , cs))
+      Just (!c, !cs) | c == _equal        -> Just (c           , (InAttrList, cs))
+      Just (!c, !cs) | isQuote c          -> Just (_parenleft  , (InString c, cs))
+      Just (!c, !cs)                      -> Just (c           , (InAttrList, cs))
       Nothing                             -> Nothing
     blankByteString (InIdent, bs) = case BS.uncons bs of
-      Just (!c, !cs) | isAlphabetic c     -> Just (_underscore , (InIdent  , cs))
-      Just (!c, !cs) | isLeadingDigit c   -> Just (_1          , (InNumber , cs))
-      Just (!c, !cs) | c == _quotedbl     -> Just (_parenleft  , (InString , cs))
-      Just (!c, !cs)                      -> Just (c           , (InXml   , cs))
+      Just (!c, !cs) | isNameChar c       -> Just (c      , (InIdent   , cs))
+      Just (!c, !cs) | isSpace c          -> Just (_parenright , (InAttrList, cs))
+      Just (!c, !cs) | c == _equal        -> Just (_parenright , (InAttrList, cs))
+      Just (_, !cs)                       -> Just (_parenright , (InAttrList, cs))
       Nothing                             -> Nothing
+    blankByteString (InString q, bs) = case BS.uncons bs of
+      Just (!c, !cs) | c == q             -> Just (_parenright , (InAttrList, cs))
+      Just (!c , !cs)                      -> Just (c      , (InString q, cs))
+      Nothing                             -> Nothing
+    blankByteString (Test, bs) = case BS.uncons bs of
+      Just (!c, !cs)                      -> Just (c , (Test, cs))
+      Nothing                             -> Nothing
+
+headIs :: (Word8 -> Bool) -> ByteString -> Maybe Bool
+headIs p bs = case BS.uncons bs of
+  Just (!c, _) -> Just (p c)
+  Nothing      -> Nothing
+{-# INLINE headIs #-}
+
+unsnocStartElem :: ByteString -> (ByteString, Maybe Word8)
+unsnocStartElem = unscnocIf (==_less)
+{-# INLINE unsnocStartElem #-}
+
+unscnocIf :: (Word8 -> Bool) -> ByteString -> (ByteString, Maybe Word8)
+unscnocIf p bs =
+  case BS.unsnoc bs of
+    Just (bs', w) | p w -> (bs', Just w)
+    _                   -> (bs , Nothing)
+{-# INLINE unscnocIf #-}
--- src/HaskellWorks/Data/Xml/Conduit/Words.hs
+++ src/HaskellWorks/Data/Xml/Conduit/Words.hs
@@ -11,3 +11,23 @@ isTrailingDigit w = w == _plus || w == _hyphen || (w >= _0 && w <= _9) || w == _
 
 isAlphabetic :: Word8 -> Bool
 isAlphabetic w = (w >= _A && w <= _Z) || (w >= _a && w <= _z)
+
+isQuote :: Word8 -> Bool
+isQuote w = w == _quotedbl || w == _quotesingle
+
+isNameStartChar :: Word8 -> Bool
+isNameStartChar w = w == _underscore || w == _colon || isAlphabetic w
+                 || w `isIn` (0xc0, 0xd6)
+                 || w `isIn` (0xd8, 0xf6)
+                 || w `isIn` (0xf8, 0x2ff)
+                 || w `isIn` (0x370,0x37d)
+
+isNameChar :: Word8 -> Bool
+isNameChar w = isNameStartChar w || w == _hyphen || w == _period
+            || w == 0xb7 || w `isIn` (0, 9)
+
+
+isIn :: Word8 -> (Word8, Word8) -> Bool
+isIn w (s, e) = w >= s && w <= e
+{-# INLINE isIn #-}
+

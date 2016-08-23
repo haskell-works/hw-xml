a70602449bed8c5e0cb9b7b8ebcb0445df90ec29
Alexey Raga <alexey.raga@gmail.com>
2016-08-23 15:50:58 +1000

    bench

--- bench/Main.hs
+++ bench/Main.hs
@@ -17,22 +17,8 @@ import           HaskellWorks.Data.FromByteString
 import           HaskellWorks.Data.Xml.Conduit
 import           HaskellWorks.Data.Xml.Conduit.Blank
 import           HaskellWorks.Data.Xml.Succinct.Cursor
-import           HaskellWorks.Data.Positioning
 import           HaskellWorks.Data.Succinct.BalancedParens.Simple
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic
 import           System.IO.MMap
-
-setupEnvBs :: Int -> IO BS.ByteString
-setupEnvBs n = return $ BS.pack (take n (cycle [maxBound, 0]))
-
-setupEnvBss :: Int -> Int -> IO [BS.ByteString]
-setupEnvBss n k = setupEnvBs n >>= \v -> return (replicate k v)
-
-setupEnvVector :: Int -> IO (DVS.Vector Word64)
-setupEnvVector n = return $ DVS.fromList (take n (cycle [maxBound, 0]))
-
-setupEnvVectors :: Int -> Int -> IO [DVS.Vector Word64]
-setupEnvVectors n k = setupEnvVector n >>= \v -> return (replicate k v)
 
 setupEnvXml :: FilePath -> IO BS.ByteString
 setupEnvXml filepath = do
@@ -43,39 +29,15 @@ setupEnvXml filepath = do
 loadXml :: BS.ByteString -> XmlCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64))
 loadXml bs = fromByteString bs :: XmlCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64))
 
-benchRankSelect :: [Benchmark]
-benchRankSelect =
-  [ env (setupEnvVector 1000000) $ \bv -> bgroup "Rank"
-    [ bench "Rank - Once"   (whnf (rank1    bv) 1)
-    , bench "Select - Once" (whnf (select1  bv) 1)
-    , bench "Rank - Many"   (nf   (map (getCount . rank1  bv)) [0, 1000..10000000])
-    ]
-  ]
+xmlToInterestBits3 :: MonadThrow m => Conduit BS.ByteString m BS.ByteString
+xmlToInterestBits3 = blankXml =$= blankedXmlToInterestBits
 
 runCon :: Conduit i [] BS.ByteString -> i -> BS.ByteString
 runCon con bs = BS.concat $ runListConduit con [bs]
 
-runCon2 :: Conduit i [] o -> [i] -> [o]
-runCon2 con is = let os = runListConduit con is in seq (length os) os
-
-runCon3 :: Conduit i [] BS.ByteString -> [i] -> [BS.ByteString]
-runCon3 con is = let os = runListConduit con is in seq (BS.length (last os)) os
-
-xmlToInterestBits3 :: MonadThrow m => Conduit BS.ByteString m BS.ByteString
-xmlToInterestBits3 = blankXml =$= blankedXmlToInterestBits
-
-benchRankXml40Conduits :: [Benchmark]
-benchRankXml40Conduits =
-  [ env (setupEnvXml "/Users/jky/Downloads/part40.xml") $ \bs -> bgroup "Xml40"
-    [ bench "Run blankXml                    "  (whnf (runCon blankXml                  ) bs)
-    , bench "Run xmlToInterestBits3          "  (whnf (runCon xmlToInterestBits3        ) bs)
-    , bench "loadXml                         "  (whnf  loadXml                            bs)
-    ]
-  ]
-
-benchRankXml80Conduits :: [Benchmark]
-benchRankXml80Conduits =
-  [ env (setupEnvXml "/Users/jky/Downloads/part80.xml") $ \bs -> bgroup "Xml40"
+benchRankXml8mbConduits :: [Benchmark]
+benchRankXml8mbConduits =
+  [ env (setupEnvXml "corpus/8mb.xml") $ \bs -> bgroup "Xml4mb"
     [ bench "Run blankXml                    "  (whnf (runCon blankXml                  ) bs)
     , bench "Run xmlToInterestBits3          "  (whnf (runCon xmlToInterestBits3        ) bs)
     , bench "loadXml" (whnf loadXml bs)
@@ -84,20 +46,13 @@ benchRankXml80Conduits =
 
 benchRankXmlBigConduits :: [Benchmark]
 benchRankXmlBigConduits =
-  [ env (setupEnvXml "/Users/jky/Downloads/78mb.xml") $ \bs -> bgroup "XmlBig"
+  [ env (setupEnvXml "corpus/105mb.xml") $ \bs -> bgroup "XmlBig"
     [ bench "Run blankXml                    "  (whnf (runCon blankXml                  ) bs)
     , bench "Run xmlToInterestBits3          "  (whnf (runCon xmlToInterestBits3        ) bs)
     , bench "loadXml" (whnf loadXml bs)
     ]
   ]
 
-benchBlankedXmlToBalancedParens :: [Benchmark]
-benchBlankedXmlToBalancedParens =
-  [ env (setupEnvXml "/Users/jky/Downloads/part40.xml") $ \bs -> bgroup "XmlBig"
-    [ bench "blankedXmlToBalancedParens2" (whnf (runCon2 blankedXmlToBalancedParens) [bs])
-    , bench "blankedXmlToBalancedParens2" (whnf (runCon3 blankedXmlToBalancedParens2) [bs])
-    ]
-  ]
-
 main :: IO ()
-main = defaultMain benchRankXml40Conduits
+--main = defaultMain benchRankXml8mbConduits
+main = defaultMain benchRankXmlBigConduits

165ca02ee9fc9e1eb765afc68c5349c096fb65d5
John Ky <newhoggy@gmail.com>
2017-07-04 14:46:10 +1000

    Add tests

--- test/HaskellWorks/Data/Xml/Succinct/Cursor/BalancedParensSpec.hs
+++ test/HaskellWorks/Data/Xml/Succinct/Cursor/BalancedParensSpec.hs
@@ -16,7 +16,16 @@ import           Test.Hspec
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.Xml.Succinct.Cursor.BalancedParensSpec" $ do
-  it "Blanking XML should not contain strange characters 2" $ do
+  it "Blanking XML should work 1" $ do
     let blankedXml = BlankedXml ["<t<t>>"]
     let bp = BitShown $ BS.concat (runListConduit (blankedXmlToBalancedParens2 =$= compressWordAsBit) (getBlankedXml blankedXml))
     bp `shouldBe` fromString "11011000"
+  it "Blanking XML should work 2" $ do
+    let blankedXml = BlankedXml
+          [ "<><><><><><><><>"
+          , "<><><><><><><><>"
+          ]
+    let bp = BitShown $ BS.concat (runListConduit (blankedXmlToBalancedParens2 =$= compressWordAsBit) (getBlankedXml blankedXml))
+    bp `shouldBe` fromString
+          "1010101010101010\
+          \1010101010101010"

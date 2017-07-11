f9f885e842250599b3494b2dad0b5c1fc69d182a
John Ky <newhoggy@gmail.com>
2017-07-11 15:31:32 +1000

    Add attribute selector

--- src/HaskellWorks/Data/Xml/Decode.hs
+++ src/HaskellWorks/Data/Xml/Decode.hs
@@ -1,7 +1,10 @@
 module HaskellWorks.Data.Xml.Decode where
 
+import Data.Monoid                       ((<>))
 import HaskellWorks.Data.Xml.DecodeError
 import HaskellWorks.Data.Xml.Value
+
+import qualified Data.Map as M
 
 class Decode a where
   decode :: Value -> Either DecodeError a
@@ -10,5 +13,11 @@ instance Decode Value where
   decode = Right
   {-# INLINE decode #-}
 
-fail :: String -> Either DecodeError a
-fail = Left . DecodeError
+failDecode :: String -> Either DecodeError a
+failDecode = Left . DecodeError
+
+(~@) :: Value -> String -> Either DecodeError String
+(~@) (XmlElement _ as _) name = case M.lookup name as of
+  Just text -> Right text
+  Nothing   -> failDecode $ "No such attribute " <> show name
+(~@) _ name = failDecode $ "Not an element whilst looking up attribute " <> show name

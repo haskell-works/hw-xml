1c272b79a0ebc646dd79fdcad97854c78013f7df
John Ky <newhoggy@gmail.com>
2017-07-11 16:46:55 +1000

    New (/*) operator

--- src/HaskellWorks/Data/Xml/Decode.hs
+++ src/HaskellWorks/Data/Xml/Decode.hs
@@ -22,3 +22,11 @@ failDecode = DecodeFailed . DecodeError
   Just text -> DecodeOk text
   Nothing   -> failDecode $ "No such attribute " <> show name
 (~@) _ name = failDecode $ "Not an element whilst looking up attribute " <> show name
+
+(/*) :: Value -> String -> DecodeResult Value
+(/*) (XmlElement _ _ cs) name = go cs
+  where go []   = failDecode $ "Unable to find element " <> show name
+        go (r:rs) = case r of
+          e@(XmlElement name' _ _) | name' == name  -> DecodeOk e
+          _                                         -> go rs
+(/*) _ name = failDecode $ "Expecting parent of element " <> show name

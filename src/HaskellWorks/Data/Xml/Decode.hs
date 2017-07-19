0074c8bbe1d19003d49dbf6dd97055273600822f
John Ky <newhoggy@gmail.com>
2017-07-19 15:36:26 +1000

    Better operators

--- src/HaskellWorks/Data/Xml/Decode.hs
+++ src/HaskellWorks/Data/Xml/Decode.hs
@@ -17,19 +17,31 @@ instance Decode Value where
 failDecode :: String -> DecodeResult a
 failDecode = DecodeFailed . DecodeError
 
-(~@) :: Value -> String -> DecodeResult String
-(~@) (XmlElement _ as _) name = case M.lookup name as of
+(@>) :: Value -> String -> DecodeResult String
+(@>) (XmlElement _ as _) n = case M.lookup n as of
   Just text -> DecodeOk text
-  Nothing   -> failDecode $ "No such attribute " <> show name
-(~@) _ name = failDecode $ "Not an element whilst looking up attribute " <> show name
+  Nothing   -> failDecode $ "No such attribute " <> show n
+(@>) _ n = failDecode $ "Not an element whilst looking up attribute " <> show n
 
-(~/) :: Value -> String -> DecodeResult Value
-(~/) (XmlElement _ _ cs) name = go cs
-  where go []   = failDecode $ "Unable to find element " <> show name
+(/>) :: Value -> String -> DecodeResult Value
+(/>) (XmlElement _ _ cs) n = go cs
+  where go []   = failDecode $ "Unable to find element " <> show n
         go (r:rs) = case r of
-          e@(XmlElement name' _ _) | name' == name  -> DecodeOk e
-          _                                         -> go rs
-(~/) _ name = failDecode $ "Expecting parent of element " <> show name
+          e@(XmlElement n' _ _) | n' == n -> DecodeOk e
+          _                               -> go rs
+(/>) _ n = failDecode $ "Expecting parent of element " <> show n
+
+-- Contextful
+
+(</>) :: DecodeResult Value -> String -> DecodeResult Value
+(</>) ma n = ma >>= (/> n)
+
+(<@>) :: DecodeResult Value -> String -> DecodeResult String
+(<@>) ma n = ma >>= (@> n)
+
+-- Deprecated
+(~@) :: Value -> String -> DecodeResult String
+(~@) = (@>)
 
 (/*) :: Value -> String -> DecodeResult Value
-(/*) = (~/)
+(/*) = (/>)

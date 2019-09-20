9d3c1081c1284f582e339f390e91ad7663117836
John Ky <newhoggy@gmail.com>
2019-09-20 21:58:19 +1000

    New query command

--- app/App/Commands/Types.hs
+++ app/App/Commands/Types.hs
@@ -3,8 +3,11 @@
 
 module App.Commands.Types
   ( DemoOptions(..)
+  , QueryOptions(..)
   ) where
 
 import GHC.Generics
 
 data DemoOptions = DemoOptions deriving (Eq, Show, Generic)
+
+data QueryOptions = QueryOptions deriving (Eq, Show, Generic)

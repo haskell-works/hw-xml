6b29b01a0d4be7d16c7ad1f645b3b55f2749df9e
John Ky <newhoggy@gmail.com>
2019-09-23 12:09:44 +1000

    New method switch for count command

--- app/App/Options.hs
+++ app/App/Options.hs
@@ -1,6 +1,9 @@
 module App.Options
   ( optionParser
+  , textOption
   ) where
+
+import Data.Text (Text)
 
 import qualified Data.Attoparsec.Text as AT
 import qualified Data.Text            as T
@@ -8,3 +11,6 @@ import qualified Options.Applicative  as OA
 
 optionParser :: AT.Parser a -> OA.Mod OA.OptionFields a -> OA.Parser a
 optionParser p = OA.option (OA.eitherReader (AT.parseOnly p . T.pack))
+
+textOption :: OA.Mod OA.OptionFields String -> OA.Parser Text
+textOption = fmap T.pack . OA.strOption

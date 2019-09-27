30218dfac0d5626d459607ab4ce6b424c7426444
John Ky <newhoggy@gmail.com>
2019-09-27 15:14:30 +1000

    Remove unused function toBalancedParens64

--- src/HaskellWorks/Data/Xml/Internal/ToIbBp64.hs
+++ src/HaskellWorks/Data/Xml/Internal/ToIbBp64.hs
@@ -4,31 +4,16 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module HaskellWorks.Data.Xml.Internal.ToIbBp64
-  ( toBalancedParens64
-  , toBalancedParens64'
+  ( toBalancedParens64'
   , toInterestBits64'
   , toIbBp64
   ) where
 
-import Control.Applicative
 import Data.ByteString                                    (ByteString)
-import Data.Word
 import HaskellWorks.Data.Xml.Internal.BalancedParens
 import HaskellWorks.Data.Xml.Internal.List
 import HaskellWorks.Data.Xml.Succinct.Cursor.BlankedXml   (BlankedXml (..))
 import HaskellWorks.Data.Xml.Succinct.Cursor.InterestBits (blankedXmlToInterestBits)
-
-import qualified Data.ByteString      as BS
-import qualified Data.Vector.Storable as DVS
-
-genBitWordsForever :: ByteString -> Maybe (Word8, ByteString)
-genBitWordsForever bs = BS.uncons bs <|> Just (0, bs)
-{-# INLINABLE genBitWordsForever #-}
-
-toBalancedParens64 :: BlankedXml -> DVS.Vector Word64
-toBalancedParens64 (BlankedXml bj) = DVS.unsafeCast (DVS.unfoldrN newLen genBitWordsForever interestBS)
-  where interestBS    = BS.concat (compressWordAsBit (blankedXmlToBalancedParens bj))
-        newLen        = (BS.length interestBS + 7) `div` 8 * 8
 
 toBalancedParens64' :: BlankedXml -> [ByteString]
 toBalancedParens64' (BlankedXml bj) = compressWordAsBit (blankedXmlToBalancedParens bj)

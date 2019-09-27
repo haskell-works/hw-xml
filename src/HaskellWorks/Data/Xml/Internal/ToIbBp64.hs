3785687d314235989d81b96d96900808c684e166
John Ky <newhoggy@gmail.com>
2019-09-27 15:13:16 +1000

    Remove unused function toInterestBits64

--- src/HaskellWorks/Data/Xml/Internal/ToIbBp64.hs
+++ src/HaskellWorks/Data/Xml/Internal/ToIbBp64.hs
@@ -5,23 +5,23 @@
 
 module HaskellWorks.Data.Xml.Internal.ToIbBp64
   ( toBalancedParens64
-  , toInterestBits64
   , toBalancedParens64'
   , toInterestBits64'
   , toIbBp64
   ) where
 
 import Control.Applicative
+import Data.ByteString                                    (ByteString)
 import Data.Word
 import HaskellWorks.Data.Xml.Internal.BalancedParens
 import HaskellWorks.Data.Xml.Internal.List
 import HaskellWorks.Data.Xml.Succinct.Cursor.BlankedXml   (BlankedXml (..))
-import HaskellWorks.Data.Xml.Succinct.Cursor.InterestBits (blankedXmlToInterestBits, genInterestForever)
+import HaskellWorks.Data.Xml.Succinct.Cursor.InterestBits (blankedXmlToInterestBits)
 
 import qualified Data.ByteString      as BS
 import qualified Data.Vector.Storable as DVS
 
-genBitWordsForever :: BS.ByteString -> Maybe (Word8, BS.ByteString)
+genBitWordsForever :: ByteString -> Maybe (Word8, ByteString)
 genBitWordsForever bs = BS.uncons bs <|> Just (0, bs)
 {-# INLINABLE genBitWordsForever #-}
 
@@ -30,16 +30,11 @@ toBalancedParens64 (BlankedXml bj) = DVS.unsafeCast (DVS.unfoldrN newLen genBitW
   where interestBS    = BS.concat (compressWordAsBit (blankedXmlToBalancedParens bj))
         newLen        = (BS.length interestBS + 7) `div` 8 * 8
 
-toBalancedParens64' :: BlankedXml -> [BS.ByteString]
+toBalancedParens64' :: BlankedXml -> [ByteString]
 toBalancedParens64' (BlankedXml bj) = compressWordAsBit (blankedXmlToBalancedParens bj)
 
-toInterestBits64 :: BlankedXml -> DVS.Vector Word64
-toInterestBits64 (BlankedXml bj) = DVS.unsafeCast (DVS.unfoldrN newLen genInterestForever interestBS)
-    where interestBS    = BS.concat (blankedXmlToInterestBits bj)
-          newLen        = (BS.length interestBS + 7) `div` 8 * 8
-
-toInterestBits64' :: BlankedXml -> [BS.ByteString]
+toInterestBits64' :: BlankedXml -> [ByteString]
 toInterestBits64' (BlankedXml bj) = blankedXmlToInterestBits bj
 
-toIbBp64 :: BlankedXml -> [(BS.ByteString, BS.ByteString)]
+toIbBp64 :: BlankedXml -> [(ByteString, ByteString)]
 toIbBp64 bj = zip (toInterestBits64' bj) (toBalancedParens64' bj)
